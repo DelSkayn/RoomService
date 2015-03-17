8271546789d1d641eabc230c174a5760be5a5ac9
DelSkayn <yoko.minsky@gmail.com>
2015-03-17 16:11:17 +0100

    Added sliding comment section

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 function setSidebarVisablity(visable){
     if(visable){
-        
+       $("rs-sidebar-collapse").hide();
+    }else {
+        $("rs-sidebar-collapse").show();
     }
 }
+
+var l = true;
+function toggleSidebarVisablity() {
+    if (l) {
+        $(".rs-content-container").css({overflow: "hidden"});
+        $(".rs-map-container").animate({left: "300px"});
+    }else{
+        $(".rs-map-container").animate({left: "0px"},function(){
+            $(".rs-content-container").css({overflow: ""});
+        });
+    }
+    l = !l;
+}
+
+
+$(document).ready(function() {
+
+    $('#collapse').click(function () {
+        toggleSidebarVisablity();
+    });
+});
